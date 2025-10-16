ba4edaef367378bf1b4bb5eeb0d5afcad0834682
5ha <36238687+5ha@users.noreply.github.com>
2025-10-16 13:08:34 +0200

    Fixed issue with BedId not being generated by the database.

--- PrecastTracker.Data/Configurations/BedConfiguration.cs
+++ PrecastTracker.Data/Configurations/BedConfiguration.cs
@@ -10,10 +10,6 @@ public class BedConfiguration : IEntityTypeConfiguration<Bed>
     {
         builder.HasKey(b => b.BedId);
 
-        // BedId is manually set from CSV data - configure to not use auto-increment
-        builder.Property(b => b.BedId)
-            .ValueGeneratedNever();
-
-        // Don't configure Pours relationship here - it's configured in PourConfiguration
+        builder.Property(b => b.BedId);
     }
 }
